dcfc3e2c31b96324c036442095ca89cbf0b8687c
Roman Matsuiev <romanmatsuiev@gmail.com>
2023-07-12 10:51:34 +0300

    init

---
+++ constants/icons.ts
@@ -0,0 +1,14 @@
+const icons = {
+  heart: require('../assets/icons/heart.png'),
+  menu: require('../assets/icons/menu.png'),
+  search: require('../assets/icons/search.png'),
+  filter: require('../assets/icons/filter.png'),
+  left: require('../assets/icons/left.png'),
+  heartOutline: require('../assets/icons/heart-ol.png'),
+  share: require('../assets/icons/share.png'),
+  location: require('../assets/icons/location.png'),
+  chevronLeft: require('../assets/icons/chevron-left.png'),
+  chevronRight: require('../assets/icons/chevron-right.png'),
+};
+
+export default icons;
